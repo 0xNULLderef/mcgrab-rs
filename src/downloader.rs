4eb8e1ef53d08fb836971382d02711c57f750b86
Konrad Czerw <redmastersuper@gmail.com>
2021-12-28 18:54:59 +0100

    crime

--- src/downloader.rs
+++ src/downloader.rs
@@ -9,50 +9,52 @@ pub struct DownloadURLs {
 	pub native: Option<String>,
 }
 
-fn getFileAndPutInFolder<P: AsRef<Path>>(
+async fn getFileAndPutInFolder<P: AsRef<Path>>(
 	url: &String,
 	path: P,
 ) -> Result<(), Box<dyn std::error::Error>> {
+	println!("getting {}", url);
+	let x = reqwest::get(url).await?;
+	println!("doing the bytes");
 	fs::write(
 		path.as_ref()
 			.join(Path::new(url.split("/").last().unwrap())),
-		block_on(block_on(reqwest::get(url))?.bytes())?,
+		x.bytes().await?,
 	)?;
+	println!("done!");
 	Ok(())
 }
 
 impl DownloadURLs {
 	pub fn download(&self) -> Result<(), Box<dyn Error>> {
-		println!("{:?} {:?}", &self.lib, &self.native);
-		match (&self.lib, &self.native) {
-			(Some(url), _) => getFileAndPutInFolder(url, "./jars/")?,
-
-			(_, Some(url)) => {
-				let dir = tempdir::TempDir::new("natives-extract")?;
-				getFileAndPutInFolder(url, &dir)?;
-				let mut zip = zip::ZipArchive::new(fs::File::open(
-					&dir.path().join(url.split("/").last().unwrap()),
-				)?)?;
-				(0..zip.len())
-					.into_iter()
-					.map(|n| -> Result<(), Box<dyn Error>> {
-						let path = zip.entry_path(n)?;
-
-						if path.extension() == Some(&OsStr::new("so")) {
-							zip.extract_file(
-								n,
-								&Path::new("./natives/").join(path.file_name().unwrap()),
-								true,
-							)?;
-						}
-
-						Ok(())
-					})
-					.collect::<Result<Vec<()>, Box<dyn Error>>>()?;
-			}
-
-			(&None, &None) => unreachable!(),
+		if let Some(url) = &self.lib {
+			block_on(getFileAndPutInFolder(url, "./jars/"))?;
 		}
+
+		if let Some(url) = &self.native {
+			let dir = tempdir::TempDir::new("natives-extract")?;
+			block_on(getFileAndPutInFolder(url, &dir))?;
+			let mut zip = zip::ZipArchive::new(fs::File::open(
+				&dir.path().join(url.split("/").last().unwrap()),
+			)?)?;
+			(0..zip.len())
+				.into_iter()
+				.map(|n| -> Result<(), Box<dyn Error>> {
+					let path = zip.entry_path(n)?;
+
+					if path.extension() == Some(&OsStr::new("so")) {
+						zip.extract_file(
+							n,
+							&Path::new("./natives/").join(path.file_name().unwrap()),
+							true,
+						)?;
+					}
+
+					Ok(())
+				})
+				.collect::<Result<(), Box<dyn Error>>>()?;
+		}
+
 		Ok(())
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use std::error::Error;
+use indicatif::ProgressIterator;
 
 use downloader::DownloadURLs;
 
@@ -15,30 +16,27 @@ async fn getJSONfromURL(url: &str) -> Result<structs::McVersion, Box<dyn Error>>
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	// let version = getJSONfromURL("https://launchermeta.mojang.com/v1/packages/cfd75871c03119093d7c96a6a99f21137d00c855/1.12.2.json").await.unwrap();
-	// // println!("{:#?}", version.libraries);
-	// version.libraries.into_iter().filter_map(|d| {
-	// 	match d.natives {
-	// 		Some(list) => {
+	let version = getJSONfromURL("http://localhost/user/1.12.2.formatted.json").await.unwrap();
+	version
+		.libraries
+		.into_iter()
+		// .progress()
+		.map(|d| -> Result<(), Box<dyn Error>> {
+			if let Some(rules) = d.rules {
 
-	// 		}
-	// 		None => {
-	// 			d.downloads.artifact.expect("must have artifact or classifiers!").url
-	// 		}
-	// 	}
-	// 	todo!()
-	// 	// if d.natives.is_none() {
-	// 	// 	Some(d.downloads.artifact.match())
-	// 	// }
-	// });
-	DownloadURLs {
-		lib: Some(
-			"https://libraries.minecraft.net/com/mojang/patchy/1.3.9/patchy-1.3.9.jar".to_string(),
-		),
-		native: Some(
-			"https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.2-nightly-20140822/lwjgl-platform-2.9.2-nightly-20140822-natives-linux.jar".to_string()
-		),
-	}
-	.download()?;
+			} else {
+				DownloadURLs {
+					lib: d.downloads.artifact.map(|a| a.url),
+					native: d
+						.downloads
+						.classifiers
+						.map(|c| c.natives_linux.map(|n| n.url))
+						.flatten(),
+				}
+				.download()?
+			}
+			Ok(())
+		})
+		.collect::<Result<(), Box<dyn Error>>>()?;
 	Ok(())
 }
